2bb59d14afb338e4dd16c7c7676075ee1de5f915
Anna Rocha <168745681+Annacrocha@users.noreply.github.com>
2025-02-19 09:17:17 -0300

    array de inteiros e use o foreach para imprimir cada elemento na tela.

---
+++ Lista 12 - Letra A.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Program
+{
+    static void Main()
+    {
+       int[]  numeros = {10,15,20,25,30,35,40,45,50,55};
+       foreach (int numero in numeros)
+       {
+         Console.WriteLine(numero);  
+       }
+    }
+}
